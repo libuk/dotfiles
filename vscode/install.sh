a9cc456d0857ef302de8d10443615793c637fa80
Daniel Liburd <daniel.liburd@gmail.com>
2019-08-06 19:39:01 +0100

    Add vscode app and extensions

---
+++ vscode/install.sh
@@ -0,0 +1,17 @@
+install() {
+  brew cask install visual-studio-code
+
+  code --install-extension eamodio.gitlens
+  code --install-extension Equinusocio.vsc-material-theme
+  code --install-extension esbenp.prettier-vscode
+  code --install-extension justusadam.language-haskell
+  code --install-extension mhutchie.git-graph
+  code --install-extension mikestead.dotenv
+  code --install-extension octref.vetur
+  code --install-extension PKief.material-icon-theme
+  code --install-extension sbrink.elm
+  code --install-extension vscodevim.vim
+  code --install-extension wix.vscode-import-cost
+}
+
+install()
