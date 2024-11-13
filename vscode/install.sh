aeb3e8fa264953f159e063a0fbf21165bffd0734
Daniel Liburd <daniel.liburd@gmail.com>
2024-11-13 15:49:39 +0000

    Update vs code installation

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,24 +1,16 @@
 install_ext() {
-  brew cask install visual-studio-code
+  brew install visual-studio-code
 
-  code --install-extension 74th.Theme-NaturalContrast-With-HC
-  code --install-extension azemoh.one-monokai
   code --install-extension eamodio.gitlens
   code --install-extension esbenp.prettier-vscode
   code --install-extension fallenwood.vimL
   code --install-extension foxundermoon.shell-format
-  code --install-extension mauve.terraform
   code --install-extension mikestead.dotenv
   code --install-extension ms-azuretools.vscode-docker
-  code --install-extension ms-python.python
-  code --install-extension ms-vscode.Go
-  code --install-extension paulmolluzzo.convert-css-in-js
   code --install-extension redhat.vscode-yaml
-  code --install-extension skyapps.fish-vscode
   code --install-extension streetsidesoftware.code-spell-checker
   code --install-extension vscode-icons-team.vscode-icons
   code --install-extension vscodevim.vim
-  code --install-extension wayou.vscode-todo-highlight
   code --install-extension wix.vscode-import-cost
 }
 
