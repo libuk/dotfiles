8e84850e058f5e2a0b24555f884e1776a2faf3bd
Daniel Liburd <dan.liburd@digital.justice.gov.uk>
2025-01-30 17:40:05 +0000

    Update nvim settings

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -8,8 +8,11 @@ set.shiftwidth=_tablength   -- indent of spaces on enter
 set.expandtab = true        -- tabs are spaces
 
 -- UI LAYOUT
+set.number = true          -- show current line number
 set.relativenumber = true  -- show relative line numbers
 set.cursorline = true      -- highlight current line
+set.hlsearch = false       -- do not highlight search terms
+set.incsearch = true       -- incrementally highlight search terms
 
 -- MISC
 set.mouse=a  -- enable mouse interactions
